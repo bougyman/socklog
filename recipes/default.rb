43bfa0857027e90cdf7819e7e04f1a0610b7a73b
TJ Vanderpoel <tj@rubyists.com>
2012-10-11 16:37:23 -0500

    separated unix log into its own recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,38 +19,3 @@ when "arch"
   node.socklog.log_user = "daemon"
 end
 
-if ["debian","ubuntu"].include? node[:platform]
-  package "socklog-run" do
-    action :install
-  end
-  # We don't like debian's name for this service, prefer just socklog
-  file File.join(node[:runit][:service_dir], "socklog-unix") do
-    action :delete
-  end
-else
-  execute "socklog-conf unix" do
-    command "socklog-conf unix #{node.socklog.runas} #{node.socklog.log_user}"
-    creates "/etc/sv/socklog-unix" # Don't bother if it already exists
-    action :run
-  end
-end
-
-execute "restart_unix_log" do
-  command "sv t #{File.join(node[:runit][:service_dir], "socklog", "log")}"
-  action :nothing
-end
-
-template "/etc/sv/socklog-unix/log/run" do
-  source "unix/run.erb"
-  mode   "750"
-  owner  node.socklog.log_user
-  group  node.socklog.log_group
-  notifies :run, "execute[restart_unix_log]"
-end
-
-link "socklog-unix" do
-  target_file File.join(node[:runit][:service_dir], "socklog")
-  to "/etc/sv/socklog-unix"
-end
-
-
---
+++ recipes/unix.rb
@@ -0,0 +1,29 @@
+include_recipe "socklog"
+
+if ["debian","ubuntu"].include? node[:platform]
+  package "socklog-run" do
+    action :install
+  end
+  # We don't like debian's name for this service, prefer just socklog
+  file File.join(node[:runit][:service_dir], "socklog-unix") do
+    action :delete
+  end
+else
+  execute "socklog-conf unix" do
+    command "socklog-conf unix #{node.socklog.runas} #{node.socklog.log_user}"
+    creates "/etc/sv/socklog-unix" # Don't bother if it already exists
+    action :run
+  end
+end
+
+socklog_log "main" do
+  name "main"
+  var_log_link "/var/log/messages"
+end
+
+link "socklog-unix" do
+  target_file File.join(node[:runit][:service_dir], "socklog")
+  to "/etc/sv/socklog-unix"
+end
+
+
