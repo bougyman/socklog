4c4e2726902208e5e5987e90e8e4e0b01c9db4ff
TJ Vanderpoel <tj@rubyists.com>
2012-10-11 13:11:17 -0500

    there is no destructive save! on node data, use #save

--- providers/log.rb
+++ providers/log.rb
@@ -25,7 +25,7 @@ action :create do
   ruby_block "add_log" do
     block do
       node.socklog[new_resource.type]['logs'] << new_resource.name
-      node.save!
+      node.save
     end
     not_if node.socklog[new_resource.type]['logs'].include? new_resource.name
     notifies :create, "template[/etc/sv/socklog-#{new_resource.type}/log/run]"
@@ -52,7 +52,7 @@ action :delete do
   ruby_block "remove_log" do
     block do
       node.socklog[new_resource.type]['logs'].delete new_resource.name
-      node.save!
+      node.save
     end
     only_if node.socklog[new_resource.type]['logs'].include? new_resource.name
     notifies :create, "template[/etc/sv/socklog-#{new_resource.type}/log/run]"
