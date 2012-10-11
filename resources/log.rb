7b097c62d4d47f8b41cbb15145fc79e132c2950f
TJ Vanderpoel <tj@rubyists.com>
2012-10-11 12:12:32 -0500

    added a default action :create

--- resources/log.rb
+++ resources/log.rb
@@ -1,3 +1,4 @@
 actions :create, :delete
+default_action :create
 attribute :type, :kind_of => String, :equal_to => ["unix", "inet", "ucspi-tcp", "klog"], :default => "unix"
 attribute :var_log_link, :kind_of => String
