329b459d924619fa3951652f8841edbc28e8eed7
TJ Vanderpoel <tj@rubyists.com>
2012-10-11 09:41:51 -0500

    fixed wrong method name for logs

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,6 +23,10 @@ if ["debian","ubuntu"].include? node[:platform]
   package "socklog-run" do
     action :install
   end
+  # We don't like debian's name for this service, prefer just socklog
+  file File.join(node[:runit][:service_dir], "socklog-unix") do
+    action :delete
+  end
 else
   execute "socklog-conf unix" do
     command "socklog-conf unix #{node.socklog.runas} #{node.socklog.loguser}"
@@ -31,14 +35,21 @@ else
   end
 end
 
+execute "restart_unix_log" do
+  command "sv t #{File.join(node[:runit][:service_dir], "socklog", "log")}"
+  action :nothing
+end
+
 template "/etc/sv/socklog-unix/log/run" do
   source "unix/run.erb"
   mode   "750"
   owner  node.socklog.loguser
+  notifies :run, "execute[restart_unix_log]"
 end
 
 link "socklog-unix" do
   target_file File.join(node[:runit][:service_dir], "socklog")
   to "/etc/sv/socklog-unix"
 end
+
 
