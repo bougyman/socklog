68a5b588a3c1b86b5ecfc444196ca703c8d5ec9c
TJ Vanderpoel <tj@rubyists.com>
2012-10-11 10:27:46 -0500

    no need for timestamp options, that is not configurable here

--- resources/log.rb
+++ resources/log.rb
@@ -1,7 +1,3 @@
 actions :create, :delete
 attribute :type, :kind_of => String, :equal_to => ["unix", "inet", "ucspi-tcp", "klog"], :default => "unix"
 attribute :var_log_link, :kind_of => String
-attribute :time_stamp_opts, :kind_of => String, :default => '-t'
-
-
-
