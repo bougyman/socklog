3e9ee8141404787061ad8bee44693e99a510b192
TJ Vanderpoel <tj@rubyists.com>
2012-10-11 23:02:08 -0500

    refactor for logs as a hash

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,8 @@
 default.socklog.runas = "nobody"
 default.socklog.log_user = "daemon"
 default.socklog.log_group = "adm"
-default.socklog.inet.logs = %w{main}
-default.socklog.unix.logs = %w{main auth cron daemon debug ftp kern local mail news syslog user}
+default.socklog.inet.logs.main = true
+%w{main auth cron daemon debug ftp kern local mail news syslog user}.each do |log|
+  default.socklog.unix.logs[log] = true
+end
 default.socklog.ucspi_tcp.port = "10116"
