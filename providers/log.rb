a0bc84f4f4b083435fd43d953d475c85a907947d
TJ Vanderpoel <tj@rubyists.com>
2012-10-30 17:51:53 -0500

    qualify File with ::File

--- providers/log.rb
+++ providers/log.rb
@@ -63,7 +63,7 @@ action :create do
           node.save
         end
         not_if { node.socklog[new_resource.type]["main"]["exclude_patterns"].include? "*.*: *:*:* #{exclusion}[*" }
-        notifies :create, "template[#{File.join(node[:runit][:sv_dir], "socklog-#{new_resource.type}", "log", "main", "main", "config")}]"
+        notifies :create, "template[#{::File.join(node[:runit][:sv_dir], "socklog-#{new_resource.type}", "log", "main", "main", "config")}]"
       end
     end
   end
